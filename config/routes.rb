c6aa0a494d85f435548a4a615431d766bbe1de6f
Kyle Van Wagenen <kvanwagenen@gmail.com>
2014-12-16 18:01:27 -0700

    Just use content blocks for now

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ SapphireCms::Engine.routes.draw do
 
   namespace :api do
     resources :content_blocks
-    resources :routes  
   end
 
-  get '*path', constraints: lambda {|req| !SapphireCms::Route.find_by_slug(req.path).nil?}, to: "templates#show"
+  get '*path', constraints: lambda {|req| !SapphireCms::ContentBlock.find_by_slug(req.path).nil?}, to: "templates#show"
 
 end
